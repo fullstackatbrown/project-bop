713dea0bc720913eecbf12d5d68202801de3fc5a
danidedona <daniela_dedona@brown.edu>
2025-02-17 18:03:39 -0500

    initial commit, feat: blue/red banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Polls from "./pages/Polls";
@@ -12,47 +7,47 @@ import QuestionSubmissions from "./pages/QuestionSubmissions";
 import Team from "./pages/Team";
 
 export default function App() {
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul className="navbar">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/polls">Polls</Link>
-                        </li>
-                        <li>
-                            <Link to="/news">News</Link>
-                        </li>
-                        <li>
-                            <Link to="/question-submissions">Question Submissions</Link>
-                        </li>
-                        <li>
-                            <Link to="/team">Team</Link>
-                        </li>
-                    </ul>
-                </nav>
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul className="navbar">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/polls">Polls</Link>
+            </li>
+            <li>
+              <Link to="/news">News</Link>
+            </li>
+            <li>
+              <Link to="/question-submissions">Question Submissions</Link>
+            </li>
+            <li>
+              <Link to="/team">Team</Link>
+            </li>
+          </ul>
+        </nav>
 
-                <Switch>
-                    <Route path="/polls">
-                        <Polls />
-                    </Route>
-                    <Route path="/news">
-                        <News />
-                    </Route>
-                    <Route path="/question-submissions">
-                        <QuestionSubmissions />
-                    </Route>
-                    <Route path="/team">
-                        <Team />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+        <Switch>
+          <Route path="/polls">
+            <Polls />
+          </Route>
+          <Route path="/news">
+            <News />
+          </Route>
+          <Route path="/question-submissions">
+            <QuestionSubmissions />
+          </Route>
+          <Route path="/team">
+            <Team />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
