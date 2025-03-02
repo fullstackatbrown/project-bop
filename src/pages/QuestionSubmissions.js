b9794a108b7ff31aac49dd081902627746faeff1
John Rathgeber <john_rathgeber@brown.edu>
2025-03-02 13:26:14 -0500

    Submit functionality done and stylized

--- src/pages/QuestionSubmissions.js
+++ src/pages/QuestionSubmissions.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { db } from './Firebase';
 import { updateDoc, doc, getDoc } from "firebase/firestore";
 
 function QuestionSubmissionPage() {
   const [inputValue, setInputValue] = useState('');
   const [hasError, setHasError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    if (submitted) {
+      const timer = setTimeout(() => {
+        setSubmitted(false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [submitted]);
 
   const handleFocus = () => {
     setHasError(false);
@@ -17,19 +27,17 @@ function QuestionSubmissionPage() {
   };
 
   const handleSubmission = async () => {
+    if (inputValue.trim() === '') return;
+    setSubmitted(true);
+    setInputValue('');
     try {
       const docRef = doc(db, "bop-questions", "questions");
       const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists() && inputValue) {
+      if (docSnap.exists()) {
         const data = docSnap.data();
         const fieldCount = Object.keys(data).length;
         const id = "question-" + fieldCount;
-
-        const newField = {
-          [id]: inputValue
-        };
-    
+        const newField = { [id]: inputValue };
         await updateDoc(docRef, newField);
       }
     } catch (error) {
@@ -38,33 +46,42 @@ function QuestionSubmissionPage() {
   };
 
   return (
-    <div className="flex justify-center mt-24 space-x-8">
+    <div className="flex justify-center mt-12 space-x-8">
       {/* Left Div */}
       <div className="w-1/3 p-4 flex items-end pb-2">
-        {/* <div className="w-8/12 ml-auto"> */}
-          <p 
-            style={{ lineHeight: '1.3' }}
-            className="text-5xl m-0 leading-none m-0">What questions do you want answered?</p>
-        {/* </div> */}
+        <p style={{ lineHeight: '1.3' }} className="text-5xl m-0 leading-none">
+          What questions do you want answered?
+        </p>
       </div>
       {/* Right Div */}
-      <div className="w-1/3 p-4 flex flex-col justify-end">
-        <p>Enter your question idea here:</p>
-        <input
-          type="text"
-          placeholder="Question"
-          value={inputValue}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          onChange={(e) => setInputValue(e.target.value)}
-          className={`w-full p-2 mt-4 border-b-2 ${hasError ? 'border-red-500' : 'border-black'} outline-none`}
-        />
-        <button
-          className="w-1/3 mt-4 p-2 bg-black text-white rounded"
-          onClick={handleSubmission}
-        >
-          Submit
-        </button>
+      <div className="w-1/3 p-4 relative">
+        <div className="flex flex-col justify-end h-64 space-y-6">
+          <p>Enter your question idea here:</p>
+          <input
+            type="text"
+            placeholder="Question"
+            value={inputValue}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={(e) => { if (e.key === 'Enter') handleSubmission(); }}
+            className={`w-full p-2 border-b-2 ${hasError ? 'border-red-500' : 'border-black'} outline-none`}
+          />
+          <button
+            className="w-1/3 p-2 bg-black text-white hover:bg-[rgb(133,133,126)] transition-colors duration-300"
+            onClick={handleSubmission}
+          >
+            Submit
+          </button>
+        </div>
+        {submitted && (
+          <p
+            className="absolute left-4 w-full text-left text-gray-600"
+            style={{ bottom: -30 }}
+          >
+            Thanks for submitting!
+          </p>
+        )}
       </div>
     </div>
   );
