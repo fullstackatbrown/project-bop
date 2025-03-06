68b25a0313abc556ceb03ec87d3afb7b8dafa406
ashleywoertz <ashley.woertz@gmail.com>
2025-03-06 17:08:32 -0500

    fix: additionally styling

--- src/MemberCard.js
+++ src/MemberCard.js
@@ -4,8 +4,8 @@ export default function MemberCard({ name, position, image }) {
     return (
         <div className="member-card">
             <img src={image || "/bop_logo.avif"} alt={name} className="member-image" />
-            <h2 className="member-name">{name}</h2>
-            <p className="member-position">{position}</p>
+            <h2 className="member-name font-bold font-avenir">{name}</h2>
+            <p className="member-position font-bold">{position}</p>
         </div>
     );
 }
