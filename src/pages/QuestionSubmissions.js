1051e18aab50d54e72a36e4946c55ea2c0574e26
Ben Kang <134336066+b3nkang@users.noreply.github.com>
2025-05-02 22:04:36 -0400

    format + correct typo

--- src/pages/QuestionSubmissions.js
+++ src/pages/QuestionSubmissions.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { db } from './Firebase';
+import React, { useState, useEffect } from "react";
+import { db } from "./Firebase";
 import { updateDoc, doc, getDoc } from "firebase/firestore";
 
 function QuestionSubmissionPage() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [hasError, setHasError] = useState(false);
   const [submitted, setSubmitted] = useState(false);
 
@@ -21,15 +21,15 @@ function QuestionSubmissionPage() {
   };
 
   const handleBlur = () => {
-    if (inputValue.trim() === '') {
+    if (inputValue.trim() === "") {
       setHasError(true);
     }
   };
 
   const handleSubmission = async () => {
-    if (inputValue.trim() === '') return;
+    if (inputValue.trim() === "") return;
     setSubmitted(true);
-    setInputValue('');
+    setInputValue("");
     try {
       const docRef = doc(db, "bop-questions", "questions");
       const docSnap = await getDoc(docRef);
@@ -48,8 +48,11 @@ function QuestionSubmissionPage() {
   return (
     <div className="flex flex-col ml-10 md:ml-0 md:flex-row justify-center mt-12 space-x-0 md:space-x-8 lg:space-x-16">
       {/* Left Div */}
-      <div className="w-4/5 md:w-1/3 p-4 flex pb-2 justify-end items-end">
-        <p style={{ lineHeight: '1.3' }} className="text-3xl md:text-4xl lg:text-5xl">
+      <div className="w-[80%] md:w-[35%] p-4 flex pb-10 justify-end items-end">
+        <p
+          style={{ lineHeight: "1.3" }}
+          className="text-3xl md:text-4xl lg:text-5xl font-semibold"
+        >
           What questions do you want answered?
         </p>
       </div>
@@ -64,8 +67,12 @@ function QuestionSubmissionPage() {
             onFocus={handleFocus}
             onBlur={handleBlur}
             onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={(e) => { if (e.key === 'Enter') handleSubmission(); }}
-            className={`w-full p-2 border-b-2 ${hasError ? 'border-red-500' : 'border-black'} outline-none`}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") handleSubmission();
+            }}
+            className={`w-full p-2 border-b-2 ${
+              hasError ? "border-red-500" : "border-black"
+            } outline-none`}
           />
           <button
             className="w-1/3 p-2 bg-black text-white hover:bg-[rgb(133,133,126)] transition-colors duration-300"
