1728252a909dad9589b40d4f5249be4fcccab327
Ben Kang <134336066+b3nkang@users.noreply.github.com>
2025-05-02 21:33:39 -0400

    format footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,6 @@ export default function Footer() {
   return (
     <footer className="text-black py-6 mt-auto">
       <div className="mx-auto px-10 flex flex-col md:flex-row justify-between items-center">
-
         {/* Copyright */}
         <div className="text-center md:text-left mt-4 md:mt-0">
           <p className="text-sm">© 2025 Brown Opinion Project </p>
@@ -10,17 +9,38 @@ export default function Footer() {
 
         {/* Links to Socials */}
         <div className="flex space-x-6 mt-4 md:mt-0">
-        {/* Twitter */}
-          <a href="https://x.com/brownu_opinion" className="flex items-center justify-center text-center text-white bg-black rounded-full hover:text-blue-500 w-10 h-10">
-            <img src="/twitter-icon.png" alt="instagram icon" className="w-1/3 h-auto"/>
+          {/* Twitter */}
+          <a
+            href="https://x.com/brownu_opinion"
+            className="flex items-center justify-center text-center text-white bg-black rounded-full hover:text-blue-500 w-10 h-10"
+          >
+            <img
+              src="/twitter-icon.png"
+              alt="instagram icon"
+              className="w-1/3 h-auto"
+            />
           </a>
           {/* TikTok */}
-          <a href="https://www.tiktok.com/@brownopinionproject" className="flex items-center justify-center text-center text-white bg-black rounded-full hover:text-blue-500 w-10 h-10">
-            <img src="/tiktok-icon.png" alt="instagram icon" className="w-1/3 h-auto"/>    
+          <a
+            href="https://www.tiktok.com/@brownopinionproject"
+            className="flex items-center justify-center text-center text-white bg-black rounded-full hover:text-blue-500 w-10 h-10"
+          >
+            <img
+              src="/tiktok-icon.png"
+              alt="instagram icon"
+              className="w-1/3 h-auto"
+            />
           </a>
           {/* Instagram */}
-          <a href="https://www.instagram.com/brownopinionproject/?hl=en" className="flex items-center justify-center text-center bg-black rounded-full hover:text-blue-500 w-10 h-10">
-            <img src="/instagram-icon.png" alt="instagram icon" className="w-1/3 h-auto"/>
+          <a
+            href="https://www.instagram.com/brownopinionproject/?hl=en"
+            className="flex items-center justify-center text-center bg-black rounded-full hover:text-blue-500 w-10 h-10"
+          >
+            <img
+              src="/instagram-icon.png"
+              alt="instagram icon"
+              className="w-1/3 h-auto"
+            />
           </a>
         </div>
       </div>
