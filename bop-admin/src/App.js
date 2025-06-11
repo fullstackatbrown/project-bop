d4b00629147bc96bae85c5e8fc11e72786b7836b
sjuknelis <sjuknelis24@nobles.edu>
2025-06-11 17:48:33 -0400

    set up news list

--- bop-admin/src/App.js
+++ bop-admin/src/App.js
@@ -1,8 +1,13 @@
-import { Button } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewsList from "./pages/NewsList";
 
 function App() {
     return (
-        <Button variant="primary">test</Button>
+        <Router>
+            <Routes>
+                <Route path="/news" element={(<NewsList />)} />
+            </Routes>
+        </Router>
     );
 }
 
