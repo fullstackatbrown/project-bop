4fed9e461bd299d6818f90c08ec5f1a441e141c8
sjuknelis <sjuknelis24@nobles.edu>
2025-06-12 18:08:34 -0400

    add poll group creator

--- bop-admin/src/App.js
+++ bop-admin/src/App.js
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NewsList from "./pages/NewsList";
 import NewsEdit from "./pages/NewsEdit";
+import PollList from "./pages/PollList";
+import PollEdit from "./pages/PollEdit";
 
 function App() {
     return (
@@ -9,6 +11,8 @@ function App() {
             <Routes>
                 <Route path="/news" element={(<NewsList />)} />
                 <Route path="/news/:id" element={(<NewsEdit />)} />
+                <Route path="/poll" element={(<PollList />)} />
+                <Route path="/poll/:id" element={(<PollEdit />)} />
             </Routes>
         </Router>
     );
