7f53931947b6faa41ce16d0c131603b2b7d5594a
sjuknelis <simonas_juknelis@brown.edu>
2025-06-13 20:57:54 -0400

    add lockout mechanism

--- bop-admin/src/App.js
+++ bop-admin/src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import NewsList from "./pages/NewsList";
 import NewsEdit from "./pages/NewsEdit";
 import PollList from "./pages/PollList";
@@ -9,15 +9,23 @@ import Main from "./pages/Main";
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/news/:id" element={(<NewsEdit />)} />
-                <Route path="/news" element={(<NewsList />)} />
-                <Route path="/poll/:id" element={(<PollEdit />)} />
-                <Route path="/poll" element={(<PollList />)} />
-                <Route path="/" element={(<Main />)} />
-            </Routes>
+            <LockoutView>
+                <Routes>
+                    <Route path="/news/:id" element={(<NewsEdit />)} />
+                    <Route path="/news" element={(<NewsList />)} />
+                    <Route path="/poll/:id" element={(<PollEdit />)} />
+                    <Route path="/poll" element={(<PollList />)} />
+                    <Route path="/" element={(<Main />)} />
+                </Routes>
+            </LockoutView>
         </Router>
     );
+}
+
+function LockoutView({ children }) {
+    if (useLocation().search != "?abcde") return null;
+
+    return children;
 }
 
 export default App;
