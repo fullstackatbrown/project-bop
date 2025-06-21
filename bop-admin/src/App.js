3fd4e7dba58c722435c360b4c4dcc4c4f91cc064
sjuknelis <simonas_juknelis@brown.edu>
2025-06-21 02:25:36 -0400

    reorder admin imports

--- bop-admin/src/App.js
+++ bop-admin/src/App.js
@@ -1,13 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import NewsList from "./pages/news/NewsList";
 import NewsEdit from "./pages/news/NewsEdit";
-import PollList from "./pages/poll/PollList";
+import NewsList from "./pages/news/NewsList";
 import PollEdit from "./pages/poll/PollEdit";
-import Main from "./pages/Main";
+import PollList from "./pages/poll/PollList";
 import TeamEdit from "./pages/team/TeamEdit";
 import TeamList from "./pages/team/TeamList";
 import QuestionList from "./pages/QuestionList";
+import Main from "./pages/Main";
 
 function App() {
     return (
