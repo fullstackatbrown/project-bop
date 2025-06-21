36c2cfed8d64383f9f0534884da0562fcc768d65
sjuknelis <>
2025-06-21 14:30:51 -0400

    install during build

--- vercel.sh
+++ vercel.sh
@@ -4,10 +4,12 @@ echo "[!] Running custom vercel build script"
 cd $(dirname $0) &&
 
 # Build main project
+npm install &&
 npm run build &&
 
 # Build admin project
 cd bop-admin &&
+npm install &&
 npm run build &&
 
 # Put admin project inside main project
